71143e19ae5cdf979d979b5441edab85665c2b62
Jesse Shawl <jesse@jshawl.com>
2016-09-12 13:56:45 -0400

    add news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,4 +7,8 @@ class NewsController < ApplicationController
     f = Rails.root.join('news/_site/'+ params[:path]+'/index.html')
     send_file f, :disposition => 'inline'
   end
+  def css
+    f = Rails.root.join('news/_site/css/main.css')
+    send_file f, :disposition => 'inline'
+  end
 end
