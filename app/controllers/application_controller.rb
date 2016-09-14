d8ee87539131040abc3e92ef131e36a87c54da8b
Jesse Shawl <jesse@jshawl.com>
2016-09-14 10:25:58 -0400

    fixes #15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :redirect_custom_domain_from_https
 
   def get_client at
     return DropboxClient.new(at)
@@ -10,6 +11,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     User.find_by( access_token: session['access_token'] )
+  end
+
+  def redirect_custom_domain_from_https
+    if env['rack.url_scheme'] == "https"
+      host = env['HTTP_HOST'] + env['REQUEST_URI']
+      redirect_to 'http://' + host
+    end
   end
 
 end
