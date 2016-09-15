f8f2f38f0f70a4b4b9fedf2e74dda45cfb4786fc
Jesse Shawl <jesse@jshawl.com>
2016-09-15 09:44:45 -0400

    update news controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,12 +1,8 @@
 class NewsController < ApplicationController
   def index
-    render file: Rails.root.join('news/_site/index.html')
+    render file: 'news/_site/index.html'
   end
   def show
-    render file: Rails.root.join('news/_site/'+ params[:path]+'/index.html')
-  end
-  def css
-    f = Rails.root.join('news/_site/css/main.css')
-    send_file f, :disposition => 'inline'
+    render file: 'news/_site/'+ params[:path]+'/index.html'
   end
 end
