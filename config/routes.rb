f1989f17bda7ed385432e127a7af1242cec0560b
Jesse Shawl <jesse@jshawl.com>
2016-09-20 12:39:13 -0400

    fix new/

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get '/news/css/main.css', to: 'news#css'
   get '/news/:path', to: 'news#show'
   get '/news', to: 'news#index'
-  match '/*req', to: 'sites#load', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
+  match '/*req', to: 'sites#load', constraints: { subdomain: /.+/, domain: 'updog.co' }, via: [:get, :post, :put, :patch, :delete]
+  match '/*req', to: 'sites#load', constraints: { subdomain: /.+updog-staging/, domain:'herokuapp.com' }, via: [:get, :post, :put, :patch, :delete]
   get '/about', to: 'pages#about'
   get '/source', to: 'pages#source'
   get '/contact', to: 'pages#contact'
