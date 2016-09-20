539eb827c50cbda8c89c72fa36109034dda78ce2
Jesse Shawl <jesse@jshawl.com>
2016-09-20 11:52:21 -0400

    begin rescue email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   has_many :sites, :foreign_key => :uid, :primary_key => :uid
   def self.subscribe email
-    gibbon = Gibbon::Request.new(api_key: ENV['mailchimp_api_key'])
-    gibbon.lists(ENV['mailchimp_list_id']).members.create(body: {email_address: email, status: "subscribed", merge_fields: {}})
+    begin
+      gibbon = Gibbon::Request.new(api_key: ENV['mailchimp_api_key'])
+      gibbon.lists(ENV['mailchimp_list_id']).members.create(body: {email_address: email, status: "subscribed", merge_fields: {}})
+    rescue
+      logger.fatal "Failed to subscribe user - #{email}"
+    end
   end
   def self.create_with_omniauth( email, uid, name )
 	self.subscribe email
