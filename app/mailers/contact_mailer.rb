623ec780679c3000be385a4602c096da31969fbe
Jesse Shawl <jesse@jshawl.com>
2016-09-25 17:19:17 -0400

    specify reply_to

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ActionMailer::Base
-  default from: "<noreply@updog.co>"
+  default from: "noreply@updog.co"
   default to: "Jesse Shawl <jesseshawl@gmail.com>"
   def new_message(message)
     @message = message
-    mail subject: "Message from #{message['email']}"
+    mail subject: "Message from #{message['email']}", reply_to: message['email']
   end
 end
