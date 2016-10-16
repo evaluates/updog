4f5bfa9fcc5a35278955ed148dcfcb80413984c8
Jesse Shawl <jesse@jshawl.com>
2016-10-16 14:44:20 -0400

    fix broken custom domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/', to: 'sites#load', constraints: { subdomain: /.+/, domain:'updog.co' }, via: [:get, :post, :put, :patch, :delete]
+  match '/', to: 'sites#load', constraints: { subdomain: /.+/}, via: [:get, :post, :put, :patch, :delete]
   match '/', to: 'sites#load', constraints: { subdomain: /.+updog-staging/, domain:'herokuapp.com' }, via: [:get, :post, :put, :patch, :delete]
   root 'sites#index'
   get '/logout', to: 'sessions#destroy'
