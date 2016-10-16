44e98bbdda44b5ed3fcafbf557315b765b059a00
Jesse Shawl <jesse@jshawl.com>
2016-10-16 14:47:17 -0400

    fix broken assets

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/news', to: 'news#index'
   resources :subscriptions
   match '/*req', to: 'sites#load', constraints: { subdomain: /.+/, domain: 'updog.co' }, via: [:get, :post, :put, :patch, :delete]
+  match '/*req', to: 'sites#load', constraints: { subdomain: /.+/}, via: [:get, :post, :put, :patch, :delete]
   match '/*req', to: 'sites#load', constraints: { subdomain: /.+updog-staging/, domain:'herokuapp.com' }, via: [:get, :post, :put, :patch, :delete]
   get '/about', to: 'pages#about'
   get '/source', to: 'pages#source'
