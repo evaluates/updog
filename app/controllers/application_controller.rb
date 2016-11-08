1498472203c5987a329108c96101579c3fb92d0a
Jesse Shawl <jesse@jshawl.com>
2016-11-08 07:59:47 -0500

    add admin link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_admin
   def get_client at
     return DropboxClient.new(at)
   end
   def current_user
     User.find_by( access_token: session['access_token'] )
+  end
+  private
+  def set_admin
+    @admin = "admin" if current_user && current_user.email == "jesseshawl@gmail.com"
   end
 end
