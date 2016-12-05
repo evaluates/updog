2229cd9b29eb86d4ec8a5200c1a898f91eb011ee
Jesse Shawl <jesse@jshawl.com>
2016-12-05 15:50:05 -0500

    force encoding for payment notifications

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,6 +1,7 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
+    params.each { |k, v| params[k] = v.force_encoding('ISO-8859-1').encode('UTF-8') }
     PaymentNotification.create!(:params => params, :user_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
     User.find(params[:invoice]).create_upgrading! source: 'paypal'
     render :nothing => true
