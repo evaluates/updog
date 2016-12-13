8f2feaaad239d544b924e2b6d79b7983e62c117c
Jesse Shawl <jesse@jshawl.com>
2016-12-12 19:13:33 -0500

    space image

--- app/models/site.rb
+++ app/models/site.rb
@@ -90,7 +90,7 @@ class Site < ActiveRecord::Base
 
   def injectee
     href = "https://updog.co/?utm_source=#{self.name}&utm_campaign=stamp"
-    "<div style='position:relative;font-family:sans-serif;'><a style='padding:.5em .5em;position:absolute;margin-bottom:-2px;bottom:0;border: 2px solid black;right:15px;font-size:.75em;background:#fff;display:flex;align-items:center;text-decoration:none;border-radius:5px 5px 0 0;' href='#{href}'><img style='display:inline-block;height:15px;border-radius:5px 0 0 0 ;' src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/8431/logo_copy.png\" alt=\"\" /><span style='color:#000;display:inline-block;line-height:1;'> Hosted on UpDog</span></a></div>"
+    "<div style='position:relative;font-family:sans-serif;'><a style='padding:.25em .5em;position:absolute;margin-bottom:-2px;bottom:0;border: 2px solid black;right:15px;font-size:.75em;background:#fff;display:flex;align-items:center;text-decoration:none;border-radius:5px 5px 0 0;' href='#{href}'><img style='display:inline-block;height:15px;border-radius:5px 0 0 0 ;margin-right:.5em' src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/8431/logo_copy.png\" alt=\"\" /><span style='color:#000;display:inline-block;line-height:1;'> Hosted on UpDog</span></a></div>"
   end
 
   def domain_isnt_updog
