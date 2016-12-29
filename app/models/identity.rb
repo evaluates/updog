7cfffa21c14fb9266a075c999e424a60ea8ed081
Jesse Shawl <jesse@jshawl.com>
2016-12-29 15:55:00 -0500

    only subscribe first identity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,15 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
+  after_create :subscribe
+  def subscribe
+    if user.identities.length < 2 # if the first one
+      begin
+        Drip.subscribe email
+      rescue => e
+        logger.error "Failed to subscribe user - #{email}"
+        logger.error e.message
+        logger.error e.backtrace.join("\n")
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,6 @@ class User < ActiveRecord::Base
   has_many :sites
   has_many :identities
   has_one :upgrading
-  after_create :subscribe
-  def subscribe
-    begin
-      Drip.subscribe email
-      gibbon = Gibbon::Request.new(api_key: ENV['mailchimp_api_key'])
-      gibbon.lists(ENV['mailchimp_list_id']).members.create(body: {email_address: email, status: "subscribed", merge_fields: {}})
-    rescue => e
-      logger.error "Failed to subscribe user - #{email}"
-      logger.error e.message
-      logger.error e.backtrace.join("\n")
-    end
-  end
   def blacklisted?
     email_without_dots = self.email.gsub(/\./,'')
     ENV['blacklist'] ||= ''
